11d764fc54eaab5f992cab4e466def5a3038a39f
Gunter Königsmann <gunter@peterpall.de>
2022-03-05 17:33:28 +0100

    The headers for moving a chapter in and out one level in the TOC

--- src/cells/GroupCell.h
+++ src/cells/GroupCell.h
@@ -312,6 +312,17 @@ public:
   //! @}
   bool IsMainInput(Cell *active) const;
 
+  //! Can this chapter/section/... be made a section/subsection/... changing its subheadings, too?
+  bool SectioningCanMoveIn() const;
+  //! Can this section/subsection/... be made a chapter/section/... changing its subheadings, too?
+  bool SectioningCanMoveOut() const;
+  //! Get the end of this section/subsection/...
+  GroupCell *SectioningUnitGetEndOf() const;
+  //! Make this chapter/section/... a section/subsection/... changing its subheadings, too.
+  bool SectioningMoveIn();
+  //! Make this section/subsection/... a chapter/section/... changing its subheadings, too.
+  bool SectioningMoveOut();
+  
   //!  Return this cell's section- or image number.
   void Number(int &section, int &subsection, int &subsubsection, int &heading5, int &heading6, int &image) const;
 
